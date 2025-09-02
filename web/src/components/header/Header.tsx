79a31b782c3cc0b973ea9c16430c35630c7b4f85
gsousa12 <wgsousa12@gmail.com>
2025-09-02 10:57:37 -0300

    feat: Implement logout functionality with error handling and loading state in Header component

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,16 +1,27 @@
 import { HeaderWrapper } from "../wrappers/header-wrapper/HeaderWrapper";
-import { logoutDispatch } from "@/api/dispatchs/auth/auth.dispatchs";
 import { useAuthContext } from "../providers/auth-provider/AuthProvider";
 import { useAuthStore } from "@/stores/auth/auth.store";
 import { Button } from "../Button/Button";
-import { LogOut } from "lucide-react";
+import { CircleX, LogOut } from "lucide-react";
 import { getUserRoleLabel } from "@/common/utils/convert";
+import { useApiMutation } from "@/api/dispatchs/hooks";
+import { api } from "@/api/axios";
+import { Loader } from "../loader/Loader";
+import { AlertPopup } from "../popups/alert-popup/AlertPopup";
+import { useState } from "react";
 
 export const Header = () => {
+  const [openAlertPopup, setOpenAlertPopup] = useState<boolean>(false);
+  const {
+    mutateAsync: submitLogout,
+    isPending,
+    isError,
+    error,
+  } = useApiMutation<any, null>(() => api.post("auth/signout"));
   const logout = useAuthContext().logout;
   const handleLogout = async () => {
     try {
-      await logoutDispatch();
+      await submitLogout(null);
       logout();
     } catch (error) {
       alert("Logout failed, try again.");
@@ -28,6 +39,17 @@ export const Header = () => {
           <LogOut />
         </Button>
       </div>
+      {isPending && <Loader />}
+      {isError && (
+        <AlertPopup
+          open={openAlertPopup}
+          title="Erro ao fazer logout"
+          description={error?.message}
+          icon={<CircleX />}
+          confirmLabel="Ok"
+          onConfirm={() => setOpenAlertPopup(false)}
+        />
+      )}
     </HeaderWrapper>
   );
 };
